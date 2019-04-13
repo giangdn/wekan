e6f88668cf5e4b4c1d68855f078ff608ac5ec080
Ngọc Giang <giang.daongoc@aipacific.vn>
2019-04-13 17:41:14 +0700

    correct style for mobile display

--- client/components/boards/boardBody.js
+++ client/components/boards/boardBody.js
@@ -264,7 +264,7 @@ BlazeComponent.extendComponent({
   calendarOptions() {
     return {
       id: 'calendar-view',
-      defaultView: 'agendaDay',
+      defaultView: 'month',
       editable: true,
       timezone: 'local',
       header: {
