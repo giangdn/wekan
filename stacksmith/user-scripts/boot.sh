0dc0ebc9e912103c4b7e2f7a071b1ec12cb16792
Ngọc Giang <giang.appaz@gmail.com>
2019-06-01 19:14:17 +0700

    update member style on minicard

--- stacksmith/user-scripts/boot.sh
+++ stacksmith/user-scripts/boot.sh
@@ -9,7 +9,7 @@ readonly CONF=/build/env.config
 
 cat >"${CONF}" <<'EOF'
 export MONGO_URL=mongodb://{{DATABASE_USER}}:{{DATABASE_PASSWORD}}@{{DATABASE_HOST}}:{{DATABASE_PORT}}/{{DATABASE_NAME}}
-export ROOT_URL=http://localhost
+export ROOT_URL=https://board.teama.io
 export PORT=3000
 EOF
 
